24b3c022d0cbcfd13e386186dcdeebd2381f89f1
Eric Olson <ericolsonnv@gmail.com>
2019-08-16 10:41:22 -0700

    Added flag to node command to support sort

--- cmd/node.go
+++ cmd/node.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"kt/pkg/labrador"
 
 	"github.com/spf13/cobra"
 )
+
+var sortFlag string
 
 // nodeCmd represents the node command
 var nodeCmd = &cobra.Command{
@@ -13,11 +16,33 @@ var nodeCmd = &cobra.Command{
 	Long:  ``,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		if cmd.Flags().Changed("sort") {
+			if err := checkSortFlag(); err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		}
 		pods := labrador.FetchNode(args[0])
 		labrador.PrettyPrint(pods)
 	},
 }
 
+func checkSortFlag() (err error) {
+	switch sortFlag {
+	case "name":
+		return nil
+	case "memory":
+		return nil
+	case "cpu":
+		return nil
+	case "":
+		return nil
+	default:
+		return fmt.Errorf("Error: sort specified does not match [name|memory|cpu]")
+	}
+}
+
 func init() {
+	nodeCmd.Flags().StringVarP(&sortFlag, "sort", "s", "", "sort output by [name|memory|cpu]")
 	rootCmd.AddCommand(nodeCmd)
 }
